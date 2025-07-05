48537fa424cc76306e666bfdaa3c027f9ef95d23
Zanixpy <zanixwo@gmail.com>
2025-07-05 20:55:39 +0200

    Start of my fake piscine for 42

---
+++ codebase/ex01/ft_print_alphabet.c
@@ -0,0 +1,23 @@
+#include <unistd.h>
+
+void ft_print_alphabet(void);
+
+
+void ft_print_alphabet(void)
+{
+	char	c;
+	c = 'a';
+
+	while(c<='z')
+	{
+		write(1,&c,1);
+		c++;
+	}
+	return;
+}
+
+int main(void)
+{
+	ft_print_alphabet();
+	return 0;
+}
---
+++ codebase/ex05/ft_print_comb.c
@@ -0,0 +1,38 @@
+#include <unistd.h>
+
+void ft_print_comb(void);
+
+
+void ft_print_comb(void)
+{
+	char c[4];
+	c[0]='0';
+	c[1]='1';
+	c[2]='2';
+	while( c[0] <='7' )
+	{
+		while( c[1] <='8' )
+		{
+			while( c[2] <= '9')
+			{
+				write(1,c,3);
+				write(1,", ",2);
+				c[2]++;
+			}
+			c[1]++;
+			c[2]= c[1]+1;
+		}
+		c[0]++;
+		c[1]=c[0]+1;
+		c[2]=c[1]+1;
+	}
+}
+
+int main(void)
+{
+	ft_print_comb();
+	return 0;
+}
+
+
+
